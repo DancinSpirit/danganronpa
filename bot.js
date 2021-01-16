f3e3c7fc979b7afab2f77375e3c61898e3fcd027
Maiki <maiki.moseley@gmail.com>
2021-01-15 23:59:03 -0600

    bot

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ bot.on("ready", ()=>{
 });
 
 const reminder = new CronJob('0 0 0 * * *', function(){
-    bot.channels.cache.get('799874873714802739').send(`<@&${660664223625641994}> This is an end of day reminder reminding you that it's your turn! Do note that this is currently a test message, but it should still technically be true! ^_^`);
+    bot.channels.cache.get('799874873714802739').send(`<@&${'660664223625641994'}> This is an end of day reminder reminding you that it's your turn! Do note that this is currently a test message, but it should still technically be true! ^_^`);
 })
 reminder.start();
 
