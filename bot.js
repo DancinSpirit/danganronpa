a608eaafdea9acf6ad8564ffffd761f17a2baddd
Maiki <maiki.moseley@gmail.com>
2021-01-15 23:43:18 -0600

    start job

--- bot.js
+++ bot.js
@@ -15,6 +15,7 @@ bot.on("ready", ()=>{
 const reminder = new CronJob('45 23 * * * *', function(){
     bot.channels.cache.get('799874873714802739').send("<@660664223625641994> This is an end of day reminder reminding you that it's your turn! Do note that this is currently a test message, but it should still technically be true! ^_^");
 })
+reminder.start();
 
 bot.on("message", async (msg) =>{
     let player = false;
