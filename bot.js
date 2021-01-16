94d3df09630ef92f66f954fb56e0efa09dd16450
Maiki <maiki.moseley@gmail.com>
2021-01-16 00:00:35 -0600

    test

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ bot.on("ready", ()=>{
     console.log(`Logged In as ${bot.user.tag}`)
 });
 
-const reminder = new CronJob('0 0 0 * * *', function(){
+const reminder = new CronJob('0 5 0 * * *', function(){
     bot.channels.cache.get('799874873714802739').send(`<@&${'660664223625641994'}> This is an end of day reminder reminding you that it's your turn! Do note that this is currently a test message, but it should still technically be true! ^_^`);
 })
 reminder.start();
