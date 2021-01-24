44aae6212fc286e62df65c7cf46f3cb2112b601a
Maiki <maiki.moseley@gmail.com>
2021-01-23 23:20:13 -0600

    reminder test

--- reminder.js
+++ reminder.js
@@ -1,6 +1,8 @@
-const bot = require("./bot.js");
+const bot = require("bot.js");
+
+console.log("TEST");
 
 const reminder = function(){
-    bot.guilds.cache.get('659245797333925919').channels.cache.get('799874873714802739').send(`<@&${'660664223625641994'}> This is an end of day reminder reminding you that it's your turn! Do note that this is currently a test message, but it should still technically be true! ^_^`);
+    bot.channels.cache.get('799874873714802739').send(`<@&${'660664223625641994'}> This is an end of day reminder reminding you that it's your turn! Do note that this is currently a test message, but it should still technically be true! ^_^`);
 }
 reminder();
