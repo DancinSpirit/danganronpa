2b2050e987f25b79560682b58520bb12afd10887
Maiki <maiki.moseley@gmail.com>
2021-01-23 23:47:23 -0600

    reminder testing

--- reminder.js
+++ reminder.js
@@ -1,8 +1,4 @@
 const bot = require("./bot.js");
-
-console.log("TEST");
-
-const reminder = function(){
-    bot.channels.cache.get('799874873714802739').send(`<@&${'660664223625641994'}> This is an end of day reminder reminding you that it's your turn! Do note that this is currently a test message, but it should still technically be true! ^_^`);
-}
-reminder();
+const TOKEN = process.env.TOKEN;
+bot.destroy();
+bot.login(TOKEN);
