1f3d5bc1b39e2064c07b138d04fd619b2b625042
Maiki <maiki.moseley@gmail.com>
2021-01-23 23:26:57 -0600

    remidner test

--- bot.js
+++ bot.js
@@ -7,7 +7,12 @@ const db = require("./models");
 
 bot.login(TOKEN);
 
+bot.reminder = function(){
+    bot.channels.cache.get('799874873714802739').send(`<@&${'660664223625641994'}> This is an end of day reminder reminding you that it's your turn! Do note that this is currently a test message, but it should still technically be true! ^_^`);
+}
+
 bot.on("ready", ()=>{
+    bot.reminder();
     console.log(`Logged In as ${bot.user.tag}`)
 });
 
