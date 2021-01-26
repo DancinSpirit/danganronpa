573278a037757394d3aa18fcc935ae13aaa49463
Maiki <maiki.moseley@gmail.com>
2021-01-25 21:06:42 -0600

    edits

--- public/scripts/trial.js
+++ public/scripts/trial.js
@@ -46,18 +46,6 @@ let index = -1;
 for(let x=0; x<allAudio.length; x++){
  allAudio[x].volume = 0.2; 
 }
-let music = true;
-button.addEventListener("click",()=>{
-  if(song.paused){
-  music=true;
-  song.play(); 
-  }
-  else{
-  music=false;
-  song.pause();
-  }
-})
-//I learned jquery after this point so there goes consistency! Woo!
 
 const nextLine =  async function(){
   if(song.paused&&music){
@@ -117,40 +105,7 @@ const addText = function(){
 }
 
 const specialCommand = async function(text){
-  if(text.startsWith("<OPTION>")){
-    let optionText = text.replace("<OPTION>","");
-    if(optionText.startsWith("<OPEN>")){
-      optionText = optionText.replace("<OPEN>","").split("|");
-    }
-    if(optionText.startsWith("<CHOSEN>")){
-      optionText = optionText.replace("<CHOSEN>","").split("|");
-      console.log(optionText);
-      returnText= `<p class='boxtext optionquestion'> ${optionText[0]}</p><p class = 'boxtext option'>`;
-      for(let x=1; x<optionText.length; x++){
-        if(optionText[x].startsWith(">")){
-          returnText = returnText + `<choice>${optionText[x]}</choice> `;
-        } 
-        else{
-          returnText = returnText + `|${optionText[x]}| `
-        }
-      }
-      return returnText + "</p>";
-    }
-  }
-  if(text.startsWith("<MUSIC>")){
-    song.pause();
-    song = document.getElementById(text.replace('<MUSIC>',""));
-    if(music)
-    song.play();
-    nextLine();
-    return "";
-  }
-  if(text.startsWith("<SCENE TRANSITION>")){
-    await $("body").css("background-image", `url('/visuals/${text.replace('<SCENE TRANSITION>',"")}')`);
-    console.log(text);
-    nextLine();
-    return "";
-  }
+
 }
 
 
